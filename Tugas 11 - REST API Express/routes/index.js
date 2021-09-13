08c3aa49c53459fcc56e39c74a934124047a2a06
ard17 <4rdi17@gmail.com>
2021-09-13 19:32:26 +0700

    Tugas 11 - REST API Express

---
+++ Tugas 11 - REST API Express/routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+const karyawan = require('../controller/karyawan.js')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/register', karyawan.register);
+router.post('/login', karyawan.login);
+router.get('/karyawan', karyawan.karyawan);
+router.post('/karyawan/:name/siswa', karyawan.addSiswa)
+
+
+module.exports = router;
