fa29c49546e678d5f9f45c71bdecf318e0d82516
ard17 <4rdi17@gmail.com>
2021-09-19 11:37:15 +0700

    release 0 done

---
+++ Quiz 3/database/migrations/1632025890575_movies.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Movies extends BaseSchema {
+  protected tableName = 'movies'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('title')
+      table.text('resume')
+      table.datetime('release_date')
+      table.integer('genre_id').unsigned().references('genres.id').notNullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ Quiz 3/database/migrations/1632025899996_casts.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Casts extends BaseSchema {
+  protected tableName = 'casts'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('genres_id').unsigned().references('genres.id').notNullable()
+      table.integer('actors_id').unsigned().references('actors.id').notNullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
